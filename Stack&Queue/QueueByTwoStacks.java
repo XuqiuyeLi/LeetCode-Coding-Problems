ea837db4967e96cafa72b828ad09568ab90dc120
Summer Li <xl1029@nyu.edu>
2019-03-22 01:15:57 -0400

    Feat
    Implement queue with 2 stacks

---
+++ Stack&Queue/QueueByTwoStacks.java
@@ -0,0 +1,40 @@
+public class Solution {
+  private Deque<Integer> in;
+  private Deque<Integer> out;
+  public Solution() {
+    in = new LinkedList<Integer>();
+    out = new LinkedList<Integer>();
+  }
+  
+  public Integer poll() {
+    move();
+    return out.isEmpty() ? null : out.pollFirst();
+  }
+  
+  public void offer(int element) {
+    in.offerFirst(element);
+  }
+  
+  public Integer peek() {
+    move();
+    return out.isEmpty() ? null : out.peekFirst();
+  }
+  
+  public int size() {
+    return in.size() + out.size();
+  }
+  
+  public boolean isEmpty() {
+    return in.isEmpty() && out.isEmpty();
+  }
+
+  private void move(){
+    if(out.isEmpty()){
+      while(!in.isEmpty()){
+        out.offerFirst(in.pollFirst());
+      }
+    }
+  }
+
+}
+
